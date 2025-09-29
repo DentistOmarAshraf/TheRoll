7adb980f848d15bf8da852336dd707a6284cae21
DentistOmarAshraf <omar_ashraf@live.com>
2025-09-29 14:04:46 +0300

    Testing

--- backend/src/test.ts
+++ backend/src/test.ts
@@ -1,26 +1,10 @@
-import { Sections } from "./models/schemas/sections.js";
-import { Fields } from "./models/schemas/fields.js";
-import fieldDAO from "./DAO/daos/fieldsDAO.js";
-import connectToDB from "./db.js";
+import Handlebars from "handlebars";
 
-(async () => {
-  await connectToDB();
-  const t = await fieldDAO.createNewField({
-    name: "pritinder",
-    legend: "مدعي",
-    repet: false,
-    sub: [
-      { name: "name", label: "اسم", type: "text" },
-      { name: "id", label: "رقم قومي", type: "text" },
-      {
-        name: "gender",
-        label: "جنس",
-        type: "select",
-        option: ["male", "female"],
-      },
-    ],
-  });
-  console.log(t);
-})();
+const text =`بناءا على طلب السيد {{#pritinder}}{{name}} و يحمل رقم {{id}}{{/pritinder}}
 
-export { Sections, Fields };
+{{#pritinder}}{{gender}}{{/pritinder}}`;
+const obj = { pritinder: { name: "عمر أشرف", id: "12345", gender: "male" } };
+
+const temp = Handlebars.compile(text);
+
+console.log(temp(obj));
