2f3f32e378e3c473c46f96a469c93ed75330e123
DentistOmarAshraf <omar_ashraf@live.com>
2025-10-01 12:52:59 +0300

    [edit] fields container finshed

--- frontend/src/api/fieldsApi.jsx
+++ frontend/src/api/fieldsApi.jsx
@@ -1,8 +1,8 @@
 import apiClient from "./apiClient";
 
-export default async function getAllFields(page = 1, limit = 20) {
+export default async function getAllFields(page = 1, limit = 10) {
   try {
-    const response = await apiClient.get("/fields");
+    const response = await apiClient.get(`/fields?page=${page}&limit=${limit}`);
     return response.data;
   } catch (error) {
     console.error(error);
--- frontend/src/features/TemplateCreation/FieldContainer.jsx
+++ frontend/src/features/TemplateCreation/FieldContainer.jsx
@@ -6,17 +6,13 @@ import styles from "./FieldContainer.module.css";
 import Looding from "../../assets/looding.gif";
 
 export default function FieldContainer() {
-  const [fields, setFileds] = useState([]);
+  const [fields, setFileds] = useState({ page: 1, pages: 1, listOfFields: [] });
   const [page, setPage] = useState(1);
-  const {
-    templateData,
-    setTemplateData,
-    setTemplateStructure,
-  } = useTemp();
+  const { templateData, setTemplateData, setTemplateStructure } = useTemp();
 
   useEffect(() => {
     getAllFields(page)
-      .then((res) => setFileds(res.listOfFields))
+      .then((res) => setFileds(res))
       .catch((err) => console.error(err));
   }, [page]);
 
@@ -26,26 +22,57 @@ export default function FieldContainer() {
       setTemplateData((prev) => ({ ...prev, fields: [...prev.fields, id] }));
       setTemplateStructure((prev) => ({
         ...prev,
-        fieldsButtons: fields.filter((f) => f._id === id),
+        fieldsButtons: fields.listOfFields.filter((f) => f._id === id),
       }));
     }
   };
 
+  const handleNextclick = (e) => {
+    setPage((prev) => prev + 1);
+  };
+
+  const handlePrevClick = (e) => {
+    setPage((prev) => prev - 1);
+  };
+
   return (
-    <div className={styles["fields_container"]}>
-      {fields.length == 0 && <img src={Looding} />}
-      {fields.length > 0 &&
-        fields.map((item) => (
-          <Button
-            className={"fields_container__button"}
-            id={item._id}
-            key={item._id}
-            onClick={handleClick}
-          >
-            {item.legend}
-            {item.repet && " (مكرر)"}
-          </Button>
-        ))}
+    <div className={styles.fields}>
+      <div className={styles.fields_list}>
+        {fields.listOfFields.length == 0 && <img src={Looding} />}
+        {fields.listOfFields.length > 0 &&
+          fields.listOfFields.map((item) => (
+            <Button
+              className={"fields_list__button"}
+              id={item._id}
+              key={item._id}
+              onClick={handleClick}
+            >
+              {item.legend}
+              {item.repet && " (مكرر)"}
+            </Button>
+          ))}
+      </div>
+      <div className={styles.fields_pagnation}>
+        <Button
+          className={`fields_pagnation__button ${
+            page === 1 ? "fields_pagnation__button--disabeld" : ""
+          }`}
+          onClick={handlePrevClick}
+          disabled={page === 1}
+        >
+          قبل
+        </Button>
+        <p>{page}</p>
+        <Button
+          className={`fields_pagnation__button ${
+            page === fields.pages ? "fields_pagnation__button--disabeld" : ""
+          }`}
+          onClick={handleNextclick}
+          disabled={page === fields.pages}
+        >
+          بعد
+        </Button>
+      </div>
     </div>
   );
 }
